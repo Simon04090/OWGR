dd9ff4dc4197f558aedbc04ecf1bdec0acb1aa46
Simon Kammermeier <simonkammermeier1@gmail.com>
2019-10-29 23:13:31 +0100

    Adds name to event
    The events now have their name available for display purposes

--- src/Event.java
+++ src/Event.java
@@ -12,12 +12,14 @@ public class Event {
     final Year year;
     final int yearNum;
     final int id;
+    final String name;
 
     public Event(Element week, Element year, Element event, Year endYear) {
         this.week = week != null ? Integer.parseInt(week.html()) : -1;
         this.year = year != null ? Year.of(Integer.parseInt(year.html())) : Year.now();
         this.id = event != null ? Integer.parseInt(event.child(0).attr("href").split("=")[1]) : -1;
         this.yearNum = 2 - (int) this.year.until(endYear, ChronoUnit.YEARS);
+        this.name = event.child(0).html();
     }
 
     @Override
