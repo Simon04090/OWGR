6f0acf4d89dbd2b010958070d435bd9a6fa1cdc0
Simon Kammermeier <simonkammermeier1@gmail.com>
2020-03-01 23:28:14 +0100

    Add error handling for the SQL Interactions.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,10 @@ import java.math.RoundingMode;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -28,53 +31,62 @@ public class Main {
     private static final Map<Integer, Long> playerWeightedPointsMap = new HashMap<>();
     private static final Map<Integer, Integer> playerEventCountMap = new HashMap<>();
     private static final Map<Integer, String> playerNameMap = new HashMap<>();
-    private static Connection connection;
     private static PreparedStatement eventInsertion;
     private static PreparedStatement playerInsertion;
     private static PreparedStatement pointsInsertion;
     private static PreparedStatement pointSelection;
     private static PreparedStatement playerSelection;
 
-    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws IOException {
         int endWeek = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY)).get(IsoFields.WEEK_OF_WEEK_BASED_YEAR);
         int[][] weightIndex = getWeightIndex(endWeek);
         Year endYear = Year.now();
-        Class.forName("org.h2.Driver"); //Load right driver
-        connection = DriverManager.getConnection("jdbc:h2:./db;MODE=MySQL");
-        //Create Tables (only needed once) (Assuming if the file is there the tables have been created.)
-        if(!Files.exists(Path.of("db.mv.db"))) {
-            connection.createStatement().execute("CREATE TABLE EVENT (ID INTEGER NOT NULL, NAME TEXT, WEEK INTEGER, YEAR SMALLINT);\n" +
-                    "CREATE UNIQUE INDEX EVENT_ID_UINDEX ON EVENT (ID);" +
-                    "CREATE UNIQUE INDEX PRIMARY_KEY_3 ON EVENT (ID);\n" +
-                    "ALTER TABLE EVENT ADD CONSTRAINT EVENT_PK PRIMARY KEY (ID);" +
-                    "CREATE TABLE PLAYERS (ID INTEGER NOT NULL, NAME TEXT);\n" +
-                    "CREATE UNIQUE INDEX PLAYERS_ID_UINDEX ON PLAYERS (ID);\n" +
-                    "CREATE UNIQUE INDEX PRIMARY_KEY_D ON PLAYERS (ID);" +
-                    "ALTER TABLE PLAYERS ADD CONSTRAINT PLAYERS_PK PRIMARY KEY (ID);\n" +
-                    "CREATE TABLE POINTS (EVENTID  INTEGER NOT NULL, PLAYERID INTEGER NOT NULL, POINTS BIGINT NOT NULL, CONSTRAINT POINTS_EVENT_ID_FK FOREIGN KEY (EVENTID) " +
-                    "REFERENCES EVENT(ID), CONSTRAINT POINTS_PLAYERS_ID_FK FOREIGN KEY (PLAYERID) REFERENCES PLAYERS(ID));")
-            ;
+
+        try {
+            Class.forName("org.h2.Driver"); //Load right driver
+        } catch(ClassNotFoundException e) {
+            System.err.println("Couldn't find Database driver. Printing stacktrace and then exiting...");
+            e.printStackTrace();
+            return;
         }
 
-        eventInsertion = connection.prepareStatement("INSERT IGNORE INTO EVENT values (?,?,?,?)");
-        playerInsertion = connection.prepareStatement("INSERT IGNORE INTO PLAYERS VALUES (?,?)");
-        pointsInsertion = connection.prepareStatement("INSERT IGNORE INTO POINTS VALUES (?,?,?)");
+        try(var connection = DriverManager.getConnection("jdbc:h2:./db;MODE=MySQL")) {
+            //Create Tables (only needed once) (Assuming if the file is there the tables have been created.)
+            if(!Files.exists(Path.of("db.mv.db"))) {
+                connection.createStatement().execute("CREATE TABLE EVENT (ID INTEGER NOT NULL, NAME TEXT, WEEK INTEGER, YEAR SMALLINT);\n" +
+                        "CREATE UNIQUE INDEX EVENT_ID_UINDEX ON EVENT (ID);" +
+                        "CREATE UNIQUE INDEX PRIMARY_KEY_3 ON EVENT (ID);\n" +
+                        "ALTER TABLE EVENT ADD CONSTRAINT EVENT_PK PRIMARY KEY (ID);" +
+                        "CREATE TABLE PLAYERS (ID INTEGER NOT NULL, NAME TEXT);\n" +
+                        "CREATE UNIQUE INDEX PLAYERS_ID_UINDEX ON PLAYERS (ID);\n" +
+                        "CREATE UNIQUE INDEX PRIMARY_KEY_D ON PLAYERS (ID);" +
+                        "ALTER TABLE PLAYERS ADD CONSTRAINT PLAYERS_PK PRIMARY KEY (ID);\n" +
+                        "CREATE TABLE POINTS (EVENTID  INTEGER NOT NULL, PLAYERID INTEGER NOT NULL, POINTS BIGINT NOT NULL, CONSTRAINT POINTS_EVENT_ID_FK FOREIGN KEY (EVENTID) " +
+                        "REFERENCES EVENT(ID), CONSTRAINT POINTS_PLAYERS_ID_FK FOREIGN KEY (PLAYERID) REFERENCES PLAYERS(ID));")
+                ;
+            }
 
-        pointSelection = connection.prepareStatement("SELECT * FROM POINTS WHERE EVENTID = ?");
-        playerSelection = connection.prepareStatement("SELECT NAME FROM PLAYERS WHERE ID = ?");
+            eventInsertion = connection.prepareStatement("INSERT IGNORE INTO EVENT values (?,?,?,?)");
+            playerInsertion = connection.prepareStatement("INSERT IGNORE INTO PLAYERS VALUES (?,?)");
+            pointsInsertion = connection.prepareStatement("INSERT IGNORE INTO POINTS VALUES (?,?,?)");
 
-        eventsForYear(endYear, endYear);
-        eventsForYear(endYear.minusYears(1), endYear);
-        eventsForYear(endYear.minusYears(2), endYear);
+            pointSelection = connection.prepareStatement("SELECT * FROM POINTS WHERE EVENTID = ?");
+            playerSelection = connection.prepareStatement("SELECT NAME FROM PLAYERS WHERE ID = ?");
 
-        for(Event event : eventList) {
-            analyzeEvent(event, weightIndex[event.yearNum][event.week - 1]);
+            eventsForYear(endYear, endYear);
+            eventsForYear(endYear.minusYears(1), endYear);
+            eventsForYear(endYear.minusYears(2), endYear);
+
+            for(Event event : eventList) {
+                analyzeEvent(event, weightIndex[event.yearNum][event.week - 1]);
+            }
+
+            printTable();
+
+        } catch(SQLException e) {
+            System.err.println("Some error occurred while interacting with the database. Most likely when opening/closing the connection or creating the statements.");
+            e.printStackTrace();
         }
-
-        printTable();
-
-        connection.commit();
-        connection.close();
     }
 
     /**
@@ -88,73 +100,82 @@ public class Main {
      *
      * @throws IOException if HtmlCleaner throws one when accessing the URL.
      */
-    private static void analyzeEvent(Event event, int weight) throws IOException, SQLException {
+    private static void analyzeEvent(Event event, int weight) throws IOException {
         if(weight != 0) {
-            pointSelection.setInt(1, event.id);
-            pointSelection.execute();
-            ResultSet resultSet = pointSelection.getResultSet();
-            //Try getting the points for this event out of the database if they exist, only try parsing if they aren't in the database.
-            if(resultSet.next()) {
-                do {
-                    int playerID = resultSet.getInt("PLAYERID");
-                    //These points are multiplied by 100
-                    long unweightedPoints = resultSet.getLong("POINTS");
-                    //The points are now multiplied by 100 * 10,000 = 1,000,000
-                    long weightedPoints = unweightedPoints * weight;
-                    if(weightedPoints != 0) {
-                        playerWeightedPointsMap.put(playerID, playerWeightedPointsMap.getOrDefault(playerID, 0L) + weightedPoints);
-                    }
-                    playerEventCountMap.put(playerID, playerEventCountMap.getOrDefault(playerID, 0) + 1);
-                    playerNameMap.computeIfAbsent(playerID, integer -> {
-                        try {
-                            playerSelection.setInt(1, playerID);
-                            playerSelection.execute();
-                            ResultSet set = playerSelection.getResultSet();
-                            if(set.next()) {
-                                return set.getString("NAME");
-                            }
-                            return null;
-                        } catch(SQLException e) {
-                            e.printStackTrace();
-                            return null;
+            try {
+                pointSelection.setInt(1, event.id);
+                pointSelection.execute();
+                ResultSet resultSet = pointSelection.getResultSet();
+                //Try getting the points for this event out of the database if they exist, only try parsing if they aren't in the database.
+                if(resultSet.next()) {
+                    do {
+                        int playerID = resultSet.getInt("PLAYERID");
+                        //These points are multiplied by 100
+                        long unweightedPoints = resultSet.getLong("POINTS");
+                        //The points are now multiplied by 100 * 10,000 = 1,000,000
+                        long weightedPoints = unweightedPoints * weight;
+                        if(weightedPoints != 0) {
+                            playerWeightedPointsMap.put(playerID, playerWeightedPointsMap.getOrDefault(playerID, 0L) + weightedPoints);
                         }
-                    });
-                } while(resultSet.next());
-            }
-            else {
-                var parse = Jsoup.parse(new URL("http://www.owgr.com/en/Events/EventResult.aspx?eventid=" + event.id), 10000);
-
-                int pointPos = getPointPos(parse);
-                var players = parse.select("#phmaincontent_0_ctl00_PanelCurrentEvent > table > tbody > tr > td.name > a");
-                var points = parse.select("#phmaincontent_0_ctl00_PanelCurrentEvent > table > tbody > tr > td:nth-child(" + pointPos + ")");
-
-                if(players.size() != points.size()) {
-                    throw new IllegalStateException("Size of player list does not equal size of point list for event " + event.id);
+                        playerEventCountMap.put(playerID, playerEventCountMap.getOrDefault(playerID, 0) + 1);
+                        playerNameMap.computeIfAbsent(playerID, integer -> {
+                            try {
+                                playerSelection.setInt(1, playerID);
+                                playerSelection.execute();
+                                ResultSet set = playerSelection.getResultSet();
+                                if(set.next()) {
+                                    return set.getString("NAME");
+                                }
+                                return null;
+                            } catch(SQLException e) {
+                                System.err.println("Could not retrieve name for playerID " + playerID + ".");
+                                e.printStackTrace();
+                                return null;
+                            }
+                        });
+                    } while(resultSet.next());
+                    return;
                 }
-                for(int i = 0; i < players.size(); i++) {
-                    int playerID = Integer.parseInt(players.get(i).attr("href").split("=")[1]);
-                    //The following is done with Strings to avoid rounding errors.
-                    //Add ".00", so if the number wasn't decimal we get one with two decimal places.
-                    String[] splitByDecimalPoint = (points.get(i).html() + ".00").split("\\.");
-                    //Only keep two decimal places
-                    if(splitByDecimalPoint[1].length() > 2) {
-                        splitByDecimalPoint[1] = splitByDecimalPoint[1].substring(0, 2);
-                    }
-                    //String of the points as whole number multiplied by 100 rounded down
-                    String unweightedString = splitByDecimalPoint[0] + splitByDecimalPoint[1];
-                    //If we only had one decimal place "multiply" by 10 because it was only 10 times the actual value
-                    if(splitByDecimalPoint[1].length() < 2) {
-                        unweightedString += "0";
-                    }
-                    long unweightedPoints = Long.parseLong(unweightedString);
-                    //The points are now multiplied by 100 * 10,000 = 1,000,000
-                    long weightedPoints = unweightedPoints * weight;
-                    if(weightedPoints != 0) {
-                        playerWeightedPointsMap.put(playerID, playerWeightedPointsMap.getOrDefault(playerID, 0L) + weightedPoints);
-                    }
-                    playerEventCountMap.put(playerID, playerEventCountMap.getOrDefault(playerID, 0) + 1);
-                    String playerName = players.get(i).html();
-                    playerNameMap.put(playerID, playerName);
+            } catch(SQLException e) {
+                System.err.println("The following error occured when trying to get the information for the event " + event + " out of the database. Trying to get it normally.");
+                e.printStackTrace();
+            }
+
+            var parse = Jsoup.parse(new URL("http://www.owgr.com/en/Events/EventResult.aspx?eventid=" + event.id), 10000);
+
+            int pointPos = getPointPos(parse);
+            var players = parse.select("#phmaincontent_0_ctl00_PanelCurrentEvent > table > tbody > tr > td.name > a");
+            var points = parse.select("#phmaincontent_0_ctl00_PanelCurrentEvent > table > tbody > tr > td:nth-child(" + pointPos + ")");
+
+            if(players.size() != points.size()) {
+                throw new IllegalStateException("Size of player list does not equal size of point list for event " + event.id);
+            }
+            for(int i = 0; i < players.size(); i++) {
+                int playerID = Integer.parseInt(players.get(i).attr("href").split("=")[1]);
+                //The following is done with Strings to avoid rounding errors.
+                //Add ".00", so if the number wasn't decimal we get one with two decimal places.
+                String[] splitByDecimalPoint = (points.get(i).html() + ".00").split("\\.");
+                //Only keep two decimal places
+                if(splitByDecimalPoint[1].length() > 2) {
+                    splitByDecimalPoint[1] = splitByDecimalPoint[1].substring(0, 2);
+                }
+                //String of the points as whole number multiplied by 100 rounded down
+                String unweightedString = splitByDecimalPoint[0] + splitByDecimalPoint[1];
+                //If we only had one decimal place "multiply" by 10 because it was only 10 times the actual value
+                if(splitByDecimalPoint[1].length() < 2) {
+                    unweightedString += "0";
+                }
+                long unweightedPoints = Long.parseLong(unweightedString);
+                //The points are now multiplied by 100 * 10,000 = 1,000,000
+                long weightedPoints = unweightedPoints * weight;
+                if(weightedPoints != 0) {
+                    playerWeightedPointsMap.put(playerID, playerWeightedPointsMap.getOrDefault(playerID, 0L) + weightedPoints);
+                }
+                playerEventCountMap.put(playerID, playerEventCountMap.getOrDefault(playerID, 0) + 1);
+                String playerName = players.get(i).html();
+                playerNameMap.put(playerID, playerName);
+
+                try {
                     playerInsertion.setInt(1, playerID);
                     playerInsertion.setString(2, playerName);
                     playerInsertion.execute();
@@ -162,6 +183,9 @@ public class Main {
                     pointsInsertion.setInt(2, playerID);
                     pointsInsertion.setLong(3, unweightedPoints);
                     pointsInsertion.execute();
+                } catch(SQLException e) {
+                    System.err.println("Could not insert the points/name of (" + playerID + ", " + playerName + ")");
+                    e.printStackTrace();
                 }
             }
         }
@@ -237,7 +261,7 @@ public class Main {
      *
      * @throws IOException if HtmlCleaner throws one when accessing the URL.
      */
-    private static void eventsForYear(Year year, Year referenceYear) throws IOException, SQLException {
+    private static void eventsForYear(Year year, Year referenceYear) throws IOException {
         var url = new URL("http://www.owgr.com/events?pageNo=1&pageSize=ALL&tour=&year=" + year);
         var parse = Jsoup.parse(url, 10000);
         var select = parse.select("#ctl1 > tbody > tr");
@@ -247,12 +271,17 @@ public class Main {
             var event = element.getElementById("ctl5");
             var event1 = new Event(week, year1, event, referenceYear);
             eventList.add(event1);
-            eventInsertion.setString(1, String.valueOf(event1.id));
-            eventInsertion.setString(2, event1.name);
-            eventInsertion.setString(3, String.valueOf(event1.week));
-            eventInsertion.setString(4, String.valueOf(event1.year));
-            eventInsertion.execute();
 
+            try {
+                eventInsertion.setString(1, String.valueOf(event1.id));
+                eventInsertion.setString(2, event1.name);
+                eventInsertion.setString(3, String.valueOf(event1.week));
+                eventInsertion.setString(4, String.valueOf(event1.year));
+                eventInsertion.execute();
+            } catch(SQLException e) {
+                System.err.println("Could not insert the event " + event);
+                e.printStackTrace();
+            }
         }
     }
 
